4055a92bf7575da3f7b031d8ede08024a1325dff
rmdms <remy.benjamin.dumas@gmail.com>
2019-04-17 10:55:01 +0200

    Cursor => integration + animation

--- app/scripts/World/Ground.js
+++ app/scripts/World/Ground.js
@@ -14,6 +14,22 @@ export default class Ground {
 			piece: []
 		};
 
+		this.uniforms = {
+			time: {
+				value: 0
+			},
+			mouse: {
+				value: {
+					x: 0,
+					y: 0,
+					z: 0
+				}
+			},
+			size: {
+				value: 300
+			}
+		}
+		
 		this.init();
 	}
 
@@ -31,8 +47,105 @@ export default class Ground {
 	 * @returns {THREE.Mesh}
 	 */
 	createPiece(color = '#b32b00') {
+
+		let uniforms = this.uniforms;
+		console.log(this.uniforms.time.value)
+		
 		let geom = new THREE.PlaneBufferGeometry(this.size, this.size, this.segments, this.segments);
-		let mat = new THREE.MeshLambertMaterial({color: color});
+		let mat = new THREE.ShaderMaterial({
+			uniforms,
+			vertexShader: `
+                    varying vec3 vPos;
+                    uniform float time;
+                    uniform vec3 mouse;
+                    uniform float size;
+
+                    //  https://github.com/BrianSharpe/Wombat/blob/master/  SimplexPerlin2D.glsl
+                    float SimplexPerlin2D( vec2 P ) {
+                        const float SKEWFACTOR = 0.36602540378443864676372317075294;
+                        const float UNSKEWFACTOR = 0.21132486540518711774542560974902;
+                        const float SIMPLEX_TRI_HEIGHT = 0.70710678118654752440084436210485;
+                        const vec3 SIMPLEX_POINTS = vec3( 1.0-UNSKEWFACTOR, -UNSKEWFACTOR, 1.0-2.0*UNSKEWFACTOR );
+                        P *= SIMPLEX_TRI_HEIGHT;
+                        vec2 Pi = floor( P + dot( P, vec2( SKEWFACTOR ) ) );
+                        vec4 Pt = vec4( Pi.xy, Pi.xy + 1.0 );
+                        Pt = Pt - floor(Pt * ( 1.0 / 71.0 )) * 71.0;
+                        Pt += vec2( 26.0, 161.0 ).xyxy;
+                        Pt *= Pt;
+                        Pt = Pt.xzxz * Pt.yyww;
+                        vec4 hash_x = fract( Pt * ( 1.0 / 951.135664 ) );
+                        vec4 hash_y = fract( Pt * ( 1.0 / 642.949883 ) );
+                        vec2 v0 = Pi - dot( Pi, vec2( UNSKEWFACTOR ) ) - P;
+                        vec4 v1pos_v1hash = (v0.x < v0.y) ? vec4(SIMPLEX_POINTS.xy, hash_x.y, hash_y.y) : vec4(SIMPLEX_POINTS.yx, hash_x.z, hash_y.z);
+                        vec4 v12 = vec4( v1pos_v1hash.xy, SIMPLEX_POINTS.zz ) + v0.xyxy;
+                        vec3 grad_x = vec3( hash_x.x, v1pos_v1hash.z, hash_x.w ) - 0.49999;
+                        vec3 grad_y = vec3( hash_y.x, v1pos_v1hash.w, hash_y.w ) - 0.49999;
+                        vec3 grad_results = inversesqrt( grad_x * grad_x + grad_y * grad_y ) * ( grad_x * vec3( v0.x, v12.xz ) + grad_y * vec3( v0.y, v12.yw ) );
+                        const float FINAL_NORMALIZATION = 99.204334582718712976990005025589;
+                        vec3 m = vec3( v0.x, v12.xz ) * vec3( v0.x, v12.xz ) + vec3( v0.y, v12.yw ) * vec3( v0.y, v12.yw );
+                        m = max(0.5 - m, 0.0);
+                        m = m*m;
+                        return dot(m*m, grad_results) * FINAL_NORMALIZATION;
+                    }
+
+                    float map_range(float value, float low1, float high1, float low2, float high2) {
+                        return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+                    }
+
+                    void main() {
+                        vPos = position;
+                        float distToMouse = length(position - mouse);
+                        float maxoffs = map_range(SimplexPerlin2D(uv * 2.5 + time * 0.6), -1.0, 1.0, 0.0, 15.);
+                        // float maxoffs = 15.;
+                        vPos.z += min(maxoffs * map_range(distToMouse, -20., 20., 0., 1.), maxoffs);
+                        float smoothEdge = .00001;
+                        float edges = (
+                              smoothstep(0.0, smoothEdge, uv.x)
+                              * smoothstep(0.0, smoothEdge, uv.y)
+                              * smoothstep(1.0, 1.0 - smoothEdge, uv.x)
+                              * smoothstep(1.0, 1.0 - smoothEdge, uv.y)
+                        );
+                        vPos.z *= edges;
+                        gl_Position = projectionMatrix * modelViewMatrix * vec4(vPos, 1.0);
+                    }
+                `,
+			fragmentShader: `
+                    varying vec3 vPos;
+
+                    float when_gt(float x, float y) {
+                      return max(sign(x - y), 0.0);
+                    }
+
+                    float when_le(float x, float y) {
+                      return 1.0 - when_gt(x, y);
+                    }
+
+                    float grid(vec3 pos, vec3 axis, float size) {
+                        float width = 1.0;
+
+                        // Grid size
+                        vec3 tile = pos / size;
+
+                        // Grid centered gradient
+                        vec3 level = abs(fract(tile) - 0.5);
+
+                        // Derivative (crisp line)
+                        vec3 deri = fwidth(tile);
+
+                        vec3 grid3D = clamp(vPos, 0.0, 1.0);
+                        return grid3D.z;
+                    }
+
+                    void main() {
+                        float l = grid(vPos, vec3(1.0, 1.0, 1.0), 5.0);
+                        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+                        if (vPos.z < 0.0) discard;
+                    }
+                `,
+			extensions: {
+				derivatives: true
+			}
+		})
 
 		let ground = new THREE.Mesh(geom, mat);
 
---
+++ app/scripts/ui/cursor.js
@@ -0,0 +1,43 @@
+export default class Cursor {
+    constructor() {
+        this.obj = document.getElementById('cursor');
+        this.elems = {
+            dot: {
+                obj: document.getElementById('dot')
+            },
+            circle: {
+                obj: document.getElementById('circle')
+            }
+        };
+        this.params = {
+            color: '#fff',
+            size: 50,
+            position: {
+                x: window.innerWidth / 2,
+                y: window.innerHeight / 2
+            },
+            events: {
+                move: function (e) {
+                    this.params.position.x = e.clientX;
+                    this.params.position.y = e.clientY;
+                }
+            }
+        };
+        this.initialize();
+        this.update();
+    }
+    initialize() {
+        window.addEventListener('mousemove', this.params.events.move.bind(this), false);
+    }
+    update() {
+        requestAnimationFrame(this.update.bind(this));
+        TweenMax.to(this.elems.dot.obj, 0.25, {
+            x: (this.params.position.x - this.params.size),
+            y: (this.params.position.y - this.params.size)
+        });
+        TweenMax.to(this.elems.circle.obj, 0.5, {
+            x: (this.params.position.x - this.params.size),
+            y: (this.params.position.y - this.params.size)
+        });
+    }
+}
