0ae8c2ae924e936a5853207469f3a0b2448626b7
Mael <atzorimaltete.mael@gmail.com>
2019-06-20 01:12:00 +0200

    fixing huge bug

--- app/assets/scripts/components/environments/emotions/anger/template/River.js
+++ app/assets/scripts/components/environments/emotions/anger/template/River.js
@@ -19,9 +19,11 @@ class River {
 
 			new Stalagmite(this.groundID, this.coord, this.rule.stalagmites.radius, this.rule.stalagmites.height, this.rule.stalagmites.segments, this.rule.stalagmites.number);
 
+			/*
 			if(this.isInit) {
 				await Coral.wait(this.groundID, this.coord, this.rule.corals.max, this.rule.corals.current, true);
 			}
+			*/
 
 			resolve();
 		});
